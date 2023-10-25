0ab43c7146a8f4f0b6ed05b9339c9b542cdc6e30
MstrJrco <jeeeeekmahinay@gmail.com>
2023-10-26 00:23:44 +0800

    updated shits

---
+++ public/scripts/meals.js
@@ -0,0 +1,33 @@
+const menuToggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+  
+  menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+  });
+
+function Login() {
+  // You can set the login page URL here
+  window.location.href = "/login"; // Replace "login.html" with the actual URL of your login page
+}
+ // JavaScript function to create a star rating
+ function createStarRating(containerId, rating) {
+  const container = document.getElementById(containerId);
+  
+  // Clear any existing content in the container
+  container.innerHTML = '';
+  
+  // Create a loop to generate the stars
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement('span');
+    star.classList.add('star', i <= rating ? 'filled' : 'empty');
+    star.innerHTML = '★'; // You can use a star icon here if available
+    container.appendChild(star);
+  }
+}
+
+// Call the function with the desired rating (e.g., 4 out of 5 stars)
+createStarRating('starmls', 3);
+createStarRating('starmls1', 4);
+createStarRating('starmls2', 5);
+createStarRating('starmls3', 5);
+createStarRating('starmls4', 3);
