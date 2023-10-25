b2dbc8de5b4576f3189f2878807ef7cbc4763639
dexzea22 <77290837+dexzea22@users.noreply.github.com>
2023-10-26 00:33:32 +0800

    initial update

--- routes/register.js
+++ routes/register.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const validator = require('email-validator');
 const bcrypt = require('bcrypt');
-
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
@@ -12,39 +11,50 @@ router.get('/register', async (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-  const { email, password, usertype } = req.body;
+  // Updated to match the names used in the HTML form
+  const { firstName, lastName, email, password, confirmPassword, gender, address, dateOfBirth, phoneNumber, usertype } = req.body;
 
   if (!validator.validate(email)) {
     return res.render('register', { errorMessage: 'Invalid email address' });
   }
 
+  if (password !== confirmPassword) {
+    return res.render('register', { errorMessage: 'Passwords do not match.' });
+  }
+
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
   if (!passwordRegex.test(password)) {
     return res.render('register', {
-      errorMessage:
-        'Password must be: 8 characters minimum, at least one number, one lowercase letter, one uppercase letter, and one special character'
+      errorMessage: 'Password must meet the required criteria.'
     });
+  }
+
+  // Check if email already exists
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (user) {
+    return res.render('register', { errorMessage: 'Email already exists, please choose a different one' });
   }
 
   const saltRounds = 12;
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const hashedPassword = await bcrypt.hash(password, await bcrypt.genSalt(saltRounds));
 
-  const user = await prisma.user.findUnique({ where: { email } });
+  // Create new user
+  await prisma.user.create({
+    data: {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedPassword,
+      gender: gender,
+      address: address,
+      dateOfBirth: new Date(dateOfBirth), // Assumed to be in a format that can be directly used to create a Date object
+      phoneNumber: phoneNumber,
+      usertype: usertype
+    }
+  });
 
-  if (user) {
-    return res.render('register', { errorMessage: 'Email already exists, please choose a different one' });
-  } else {
-    const newUser = await prisma.user.create({
-      data: {
-        email,
-        password: hashedPassword,
-        usertype
-      }
-    });
-
-    res.redirect('/login');
-  }
+  // Redirect to the login page after successful registration
+  res.redirect('/login');
 });
 
 module.exports = router;
