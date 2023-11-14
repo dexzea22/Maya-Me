0e4a46183a63d8b57bb6e26aa147ddc8864b6ee1
dexzea22 <77290837+dexzea22@users.noreply.github.com>
2023-11-15 01:08:46 +0800

    Profile ejs update and routes
    Dexter Cutiee

--- routes/profile.js
+++ routes/profile.js
@@ -4,71 +4,73 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const bcrypt = require('bcrypt');
 
-// Handle GET request for the profile page
-router.get('/', async (req, res) => { // Removed '/profile' since this is already being used in app.use('/profile', profileRouter);
+// GET request handler for the Profile page
+router.get('/', async (req, res) => {
+  // Check if the user is not logged in
   if (!req.session.userId) {
-    return res.redirect('/login'); // Redirect to login if not authenticated
+    // Redirect to the login page if not logged in
+    return res.redirect('/login');
   }
 
   try {
-    // Fetch user details from database using session userId
+    // Find the user by the ID stored in the session
     const user = await prisma.user.findUnique({
       where: { id: req.session.userId },
     });
+
+    // If no user is found, destroy the session and redirect to login
     if (!user) {
-      // If no user found, destroy the session and redirect to login
-      req.session.destroy();
-      return res.redirect('/login');
+      req.session.destroy(() => {
+        res.clearCookie('connect.sid'); // The name 'connect.sid' is the default session cookie name. Change if different.
+        res.redirect('/login');
+      });
+    } else {
+      // Render the profile page and pass the user data to the template
+      res.render('profile', { title: 'Profile', userData: user });
     }
-    
-    // Pass the user data to the profile view
-    res.render('profile', { title: 'Profile', userData: user });
   } catch (error) {
-    res.status(500).send({ message: 'An error occurred while fetching the profile data', error });
+    console.error('Error fetching user profile:', error);
+    res.status(500).send('An error occurred while fetching the profile data');
   }
 });
 
-// Handle POST request for updating user profile
-router.post('/', async (req, res) => { // Removed '/profile' for the same reason as above
+// POST request handler for updating the user profile
+router.post('/', async (req, res) => {
   const { firstName, lastName, phoneNumber, currentPassword, newPassword } = req.body;
-  
+
   if (!req.session.userId) {
-    return res.status(401).send({ message: 'User is not logged in.' });
+    return res.status(401).send('User is not logged in.');
   }
-  
+
   try {
     const user = await prisma.user.findUnique({
       where: { id: req.session.userId },
     });
 
     if (!user) {
-      return res.status(404).send({ message: 'User not found.' });
+      return res.status(404).send('User not found.');
     }
 
-    // Check if the current password matches
-    if (currentPassword && !await bcrypt.compare(currentPassword, user.password)) {
-      return res.status(401).send({ message: 'Current password is incorrect' });
-    }
+    let updateData = { firstName, lastName, phoneNumber };
 
-    // Update password if newPassword is provided and currentPassword is correct
-    let updateData = {
-      firstName: firstName || user.firstName, // Fallback to current name if not provided
-      lastName: lastName || user.lastName,
-      phoneNumber: phoneNumber || user.phoneNumber,
-    };
-
-    if (newPassword) {
+    // Check if the current password is provided for password update
+    if (currentPassword && newPassword) {
+      const passwordValid = await bcrypt.compare(currentPassword, user.password);
+      if (!passwordValid) {
+        return res.status(401).send('Current password is incorrect.');
+      }
       updateData.password = await bcrypt.hash(newPassword, 10);
     }
 
+    // Update the user profile
     await prisma.user.update({
       where: { id: req.session.userId },
       data: updateData,
     });
 
-    res.send({ message: 'Profile updated successfully' });
+    res.send('Profile updated successfully.');
   } catch (error) {
-    res.status(500).send({ message: 'An error occurred while updating the profile', error: error.message });
+    res.status(500).send('An error occurred while updating the profile.');
   }
 });
 
